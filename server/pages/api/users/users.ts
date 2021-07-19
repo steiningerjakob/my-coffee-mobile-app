01fa8cd28dbf361eb64b861ca9bfe5b35dea78c6
“Jakob <steiningerjakob@gmail.com>
2021-07-19 22:03:47 +0200

    Test Heroku

--- server/pages/api/users/users.ts
+++ server/pages/api/users/users.ts
@@ -3,7 +3,11 @@ import { getAllUsers } from '../../../util/database';
 import { User } from '../../../util/types';
 
 export default async function usersHandler(res: NextApiResponse) {
-  const users: User = await getAllUsers();
+  const users: User[] = await getAllUsers();
 
-  return res.status(200).json({ users: users });
+  if (users) {
+    return res.status(200).json({ users: users });
+  } else {
+    return res.status(400).json({ error: 'No users found' });
+  }
 }
