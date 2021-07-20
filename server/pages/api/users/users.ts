de5047cd3a17bf3dd6c5eba69611cfc50f41190c
“Jakob <steiningerjakob@gmail.com>
2021-07-20 09:34:39 +0200

    Test Heroku

--- server/pages/api/users/users.ts
+++ server/pages/api/users/users.ts
@@ -1,13 +1,18 @@
-import { NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { getAllUsers } from '../../../util/database';
 import { User } from '../../../util/types';
 
-export default async function usersHandler(res: NextApiResponse) {
-  const users: User[] = await getAllUsers();
+export default async function usersHandler(
+  req: NextApiRequest,
+  res: NextApiResponse,
+) {
+  if (req.method === 'GET') {
+    const users: User[] = await getAllUsers();
 
-  if (users) {
-    return res.status(200).json({ users: users });
-  } else {
-    return res.status(400).json({ error: 'No users found' });
+    if (users) {
+      return res.status(200).json({ users: users });
+    } else {
+      return res.status(400).json({ error: 'No users found' });
+    }
   }
 }
